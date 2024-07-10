12386be0d8accb87a09f30813369bbbf89f8b989
Kumar123Raju <rjrajujames123@gmail.com>
2024-07-11 01:36:23 +0530

    i have remove that i used directly EmployeeEntites to EmployeDto

--- src/main/java/com/lecture21/lecture21/controller/EmployeController.java
+++ src/main/java/com/lecture21/lecture21/controller/EmployeController.java
@@ -3,6 +3,7 @@ package com.lecture21.lecture21.controller;
 import com.lecture21.lecture21.dto.EmployeDto;
 import com.lecture21.lecture21.entities.EmployeEntities;
 import com.lecture21.lecture21.service.EmployeeService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,23 +16,23 @@ public class EmployeController {
 //    i am using private final thats why used contructor injection
     private final EmployeeService employeeService;
 
+
     public EmployeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
     @GetMapping("/getEmployees/{id}")
-    public EmployeEntities getEmployee(@PathVariable Integer id) {
-
-        return employeeService.getEmployee(id);
+    public EmployeDto getEmployee(@PathVariable Integer id) {
+       return employeeService.getEmployee(id);
 
     }
     @GetMapping("/getAllEmployees")
-    public List<EmployeEntities> getAllEmployees(){
+    public List<EmployeDto> getAllEmployees(){
         return employeeService.getAllEmployee();
     }
 
     @PostMapping("/setEmployees")
-    public EmployeEntities setEmployees(@RequestBody EmployeEntities emp){
+    public EmployeDto setEmployees(@RequestBody EmployeDto emp){
          return employeeService.setEmployee(emp);
     }
 
