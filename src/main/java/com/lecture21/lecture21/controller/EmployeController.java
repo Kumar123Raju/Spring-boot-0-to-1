c84bf720f1907bd61276acdb2e98bf8cc8932043
Kumar123Raju <rjrajujames123@gmail.com>
2024-07-23 02:26:09 +0530

    In This Lecture we learn how to use input validation annotation and make our own custom annotation ex: EmployeeRoleValidation annotation

--- src/main/java/com/lecture21/lecture21/controller/EmployeController.java
+++ src/main/java/com/lecture21/lecture21/controller/EmployeController.java
@@ -7,6 +7,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -39,7 +40,7 @@ public class EmployeController {
     }
 
     @PostMapping("/setEmployees")
-    public ResponseEntity<EmployeDto> setEmployees(@RequestBody EmployeDto emp){
+    public ResponseEntity<EmployeDto> setEmployees(@RequestBody @Validated EmployeDto emp){
         EmployeDto employeDto= employeeService.setEmployee(emp);
         return new ResponseEntity<>(employeDto, HttpStatus.CREATED);
     }
--- src/main/java/com/lecture21/lecture21/dto/EmployeDto.java
+++ src/main/java/com/lecture21/lecture21/dto/EmployeDto.java
@@ -1,60 +1,37 @@
 package com.lecture21.lecture21.dto;
 
+
+import com.lecture21.lecture21.annotation.EmployeeRoleValidation;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeDto {
+
     Integer id;
+    @NotBlank(message = "Roll number not be Blanks")
+    @Size(min=3,max=20,message = "Roll number length is between 3 and 20 only")
     String rollno;
     String name;
+    @Max(value=30,message = "Age of student cannot greater than 30")
+    @Min(value=18 ,message="Age of student cannot smaller than 18")
+    Integer age;
     String collegeName;
+    @NotBlank
+//    @Pattern(regexp ="^(ADMIN|USER)$",message = "Only either ADMIN or either USER")
+    @EmployeeRoleValidation   //use interface name that used while making your interface
+    String role;   //ADMIN,USER
     String location;
+    @Email(message = "Email is Invalid")
+    String email;
+    @Max(value = 50000,message = "Salary is not more than 50K")
+    @NotNull
+    @Positive
+    @Digits(integer = 6,fraction = 2,message="Salary is allowed only in This formate: xxxxxx.yy")
+    double salary;
 
-    public EmployeDto() {
-    }
 
-    public EmployeDto(Integer id, String rollno, String name, String collegeName, String location) {
-        this.id = id;
-        this.rollno = rollno;
-        this.name = name;
-        this.collegeName = collegeName;
-        this.location = location;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getRollno() {
-        return rollno;
-    }
-
-    public void setRollno(String rollno) {
-        this.rollno = rollno;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCollegeName() {
-        return collegeName;
-    }
-
-    public void setCollegeName(String collegeName) {
-        this.collegeName = collegeName;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
 }
